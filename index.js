0412142aa45bdf4a8dfd1fb0d2ea79f01bb58db0
Gaborojers <gaborojers@gmail.com>
2024-02-29 04:47:13 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const cors = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  next();
+};
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const port = 3004;
+
+app.use(express.json());
+app.use(cors);
+
+const server = httpServer.listen(port, () => {
+  console.log(`API corriendo en el puerto ${port}`);
+});
+
+io.on("connection", (socket) => {
+  console.log("Cliente conectado");
+
+  socket.on("message", (message) => {
+    console.log("Mensaje recibido del cliente: %s", message);
+    socket.emit("mensajeServidor", message); 
+  });
+});
